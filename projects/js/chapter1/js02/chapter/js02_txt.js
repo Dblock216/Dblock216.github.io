7dd91dcf87aeb6cfe7bfb9e0e482bac7d533a1d0
Dblock216 <137729175+Dblock216@users.noreply.github.com>
2025-10-15 14:06:17 -0400

    Add files via upload

---
+++ projects/js/chapter1/js02/chapter/js02_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: A'Niya Tugard
+      Date:   October 15. 2025
+
+      Filename: js02.js
+ */
+
