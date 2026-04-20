96205cc75b45e0ba47a073e0986b205fef64f1a2
Dblock216 <137729175+Dblock216@users.noreply.github.com>
2026-04-20 11:54:45 -0400

    Add files via upload

---
+++ projects/js/chapter1/js09/chapter/js09c.js
@@ -0,0 +1,34 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+      
+      Eating Well in Season Retrieving Data from Local Storage
+      Author: 
+      Date:   
+      
+      Filename: js09c.js
+ */
+
+// array of key names
+let keys = ["name", "email","phone","city","state","zip","allergies","frequency","size"];
+      for(let item of keys){
+            let newRow=document.createElement("tr");
+            //display key
+            let keyCell = document.createElement("td");
+            keyCell.textContent = item;
+            newRow.appendChild(keyCell);
+            //display key value
+            let keyValue = document.createElement("td");
+            keyValue.textContent=localStorage.getItem(item);
+            newRow.appendChild(keyValue);
+
+            //append a row to a table
+            document.getElementById("prefTable").appendChild(newRow);
+      }
+
+      //remove preferences from local storage
+      document.getElementById("removePrefBtn").onclick=function(){
+            for(let item of keys)
+                  localStorage.removeItem(item);
+      }
